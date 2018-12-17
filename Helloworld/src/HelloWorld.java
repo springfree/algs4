b876586ae59eee2661bf2e8f220b49f23328de31
Dell <Dell@l>
2018-12-17 18:51:56 +0800

    HelloWorld

---
+++ Helloworld/src/HelloWorld.java
@@ -0,0 +1,30 @@
+import edu.princeton.cs.algs4.*;
+public class HelloWorld {
+    public static void main(String[] args) { 
+        //System.out.println("Hello, World");
+        //int N=Integer.parseInt(args[0]);
+        String s=StdIn.readLine();
+        //String s="thid";
+        StdOut.printf(s);
+        //int M=Integer.parseInt(args[1]);
+        //int L=Integer.parseInt(args[2]);
+        //int K=Integer.parseInt(args[3]);
+        //int N=3;
+        //for(int i=0;i<N;i++) StdOut.print(i);
+        /*StdOut.print(M);
+        StdOut.print(N);
+        StdOut.print(L);
+        StdOut.print('s');
+        StdOut.print(K);*/
+    	//Double sum=StdIn.readDouble();
+    	//Double s=4.3;
+    	//s=StdIn.readDouble();
+    	//StdOut.print(sum);
+    }
+}
+
+/*熟悉环境： 桌面布局  分屏  克隆  导入包  move file
+     基础复习： 基本输入  输出  
+     未解决：     命令行输入 
+     已导入stdlib
+*/
